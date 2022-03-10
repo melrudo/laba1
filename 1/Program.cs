7adea8011cb455216b12c44590f1b650ee83f9ef
Natas <andrey.meleschhko@mail.ru>
2022-03-10 12:10:18 +0300

    fff

---
+++ 1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _1
+{
+    class programm
+    {
+        void Main(string []args)
+        {
+            reka a = new reka();
+        }
+    }
+    
+    class reka
+    {
+        private string name = "none";
+        private double ploshad = 0;
+        private double protyazh = 0;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public double Ploshad
+        {
+            get { return ploshad; }
+            set { ploshad = value; }
+        }
+        public double Protyazh
+        {
+            get { return protyazh; }
+            set { protyazh = value; }
+        }
+    }
+}
