cc93d540e6da7088f7695d5a3056aa305ca86630
Natas <andrey.meleschhko@mail.ru>
2022-03-12 08:15:59 +0300

    hdgdh

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,35 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace _1
+namespace Labal1
 {
-    class programm
+    internal class Program
     {
-        void Main(string []args)
+        class Address
         {
-            reka a = new reka();
-        }
-    }
-    
-    class reka
-    {
-        private string name = "none";
-        private double ploshad = 0;
-        private double protyazh = 0;
+            public Address(string city, string street, int home) { this.city = city; this.street = street; this.home = home; }
+            public void Print() => Console.WriteLine("Название: {0}\nПлощадь бассейна: {1}\nПротяженность: {2}", city, street, home);
+            private string city = "none";
+            private string street = "none";
+            private int home = 0;
 
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
+            public string City
+            {
+                get { return city; }
+                set { city = value; }
+            }
+            public string Street
+            {
+                get { return street; }
+                set { street = value; }
+            }
+            public int Home
+            {
+                get { return home; }
+                set { home = value; }
+            }
         }
-        public double Ploshad
+
+        static void Main(string[] args)
         {
-            get { return ploshad; }
-            set { ploshad = value; }
+            Address first = new Address("Полота", "325245", 4356356);
+            first.Print();
+            Console.ReadKey();
         }
-        public double Protyazh
-        {
-            get { return protyazh; }
-            set { protyazh = value; }
-        } 
     }
 }
